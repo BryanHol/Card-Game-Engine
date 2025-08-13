8d8766001e9a3bdfaaad2ec75bc5658a096c4ca8
BryanHol <bholl020@mtroyal.ca>
2025-08-12 20:58:00 -0600

    Added a card class and fixed some of the logic in the __repr__ functions

--- Hand_Generator.py
+++ Hand_Generator.py
@@ -5,20 +5,28 @@ HAND_SIZE = 5
 
 class Deck:
 
-    def __init__(self, SUITS):
+    def __init__(self, filename):
 
         self.deck = []
-        with open("Classic_Deck.txt", "r") as file:
+
+        with open(filename, "r") as file:
+            file.readline()  # Skip the header line
             for line in file:
-                self.deck.append(line.strip("\n"))
-        self.deck.pop(0)  # Remove the header line
+                card = line.strip("\n").split(" of ")
+                card = Card(card[1], card[0])
+                self.deck.append(card)
     
-    def __len__(self):
+    def __len__(self): # Returns the number of cards left in the deck
         return len(self.deck)
     
     def __repr__(self):
-        return "This deck contains the following cards:\n" +\
-                "\n".join(self.deck)
+        string =  "This deck contains the following cards:\n"
+        for card in self.deck:
+            string += repr(card) + "\n"
+        return string
+    
+    def remove_card(self, card):
+        self.deck.remove(card)
     
 class Hand:
 
@@ -31,12 +39,34 @@ class Hand:
             while card in self.hand:
                 card = random.choice(self.deck)
             self.hand.append(card)
+        
+        # When a hand is created, remove the cards from the deck
+        for card in self.hand:
+            deck.remove_card(card)
 
     def __repr__(self):
-        return "Your hand contains the following cards:\n" +\
-                "\n".join(self.hand)
+        string = "This hand contains the following cards:\n"
+        # Convert Card objects to string representation
+        for card in self.hand:
+            if isinstance(card, Card):
+                string += repr(card) + "\n"
+        return string
 
-my_deck = Deck(CLASSIC)
+class Card:
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
+    def __repr__(self):
+        return f"{self.rank} of {self.suit}"
+    
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            return self.suit == other.suit and self.rank == other.rank
+        return False
+    
+my_deck = Deck("Classic_Deck.txt")
 my_hand = Hand(my_deck)
+print(my_deck)
 print(my_hand)
